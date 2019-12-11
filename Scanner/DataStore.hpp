8b3dd68104919363c57eca01fb763b47b03cbf2f
Bruno La Pastina <brunolap@gmail.com>
2019-12-10 23:32:45 -0300

    Added basic raw data storeage.

---
+++ Scanner/DataStore.hpp
@@ -0,0 +1,162 @@
+#pragma once
+#include <stdexcept>
+#include "sqlite3.h"
+
+static constexpr char create_table_query[]
+{
+   "CREATE TABLE IF NOT EXISTS raw_data (ip INTEGER, port MEDIUMINT, result INTEGER, response VARCHAR)"
+};
+
+static constexpr char begin_transaction_query[]
+{
+   "BEGIN"
+};
+
+static constexpr char commit_transaction_query[]
+{
+   "COMMIT"
+};
+
+static constexpr char insert_record_query[]
+{
+   "INSERT INTO raw_data (ip, port, result, response) values (?, ?, ?, ?)"
+};
+
+class DataStore
+{
+public:
+   DataStore()
+   {
+      int rc = sqlite3_open("tls_observatory.db", &m_db);
+      if (rc)
+      {
+         throw std::runtime_error(std::string("Can't open database: ") + sqlite3_errmsg(m_db));
+      }
+
+      rc = sqlite3_exec( m_db, create_table_query, nullptr, nullptr, nullptr );
+      if (rc != SQLITE_OK)
+      {
+         throw std::runtime_error(std::string("sqlite3_exec(create table) error: ") + sqlite3_errmsg(m_db));
+      }
+
+      rc = sqlite3_prepare_v2(m_db, begin_transaction_query, strlen(begin_transaction_query), &m_begin_stml, nullptr);
+      if (rc != SQLITE_OK)
+      {
+         throw std::runtime_error(std::string("sqlite3_prepare_v2(begin) error: ") + sqlite3_errmsg(m_db));
+      }
+
+      rc = sqlite3_prepare_v2(m_db, commit_transaction_query, strlen(commit_transaction_query), &m_commit_stml, nullptr);
+      if (rc != SQLITE_OK)
+      {
+         throw std::runtime_error(std::string("sqlite3_prepare_v2(commit) error: ") + sqlite3_errmsg(m_db));
+      }
+
+      rc = sqlite3_prepare_v2(m_db, insert_record_query, strlen(insert_record_query), &m_insert_stml, nullptr);
+      if (rc != SQLITE_OK)
+      {
+         throw std::runtime_error(std::string("sqlite3_prepare_v2(insert) error: ") + sqlite3_errmsg(m_db));
+      }
+   }
+
+   ~DataStore()
+   {
+      int rc = sqlite3_finalize(m_insert_stml);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_finalize(insert) error: %s\n", sqlite3_errmsg(m_db));
+      }
+
+      rc = sqlite3_finalize(m_commit_stml);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_finalize(commit) error: %s\n", sqlite3_errmsg(m_db));
+      }
+
+      rc = sqlite3_finalize(m_begin_stml);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_finalize(begin) error: %s\n", sqlite3_errmsg(m_db));
+      }
+
+      sqlite3_close(m_db);
+   }
+
+   bool begin()
+   {
+      int rc = sqlite3_step(m_begin_stml);
+      if (SQLITE_DONE != rc)
+      {
+         printf("sqlite3_step(begin) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+      }
+
+      sqlite3_clear_bindings(m_begin_stml);
+      sqlite3_reset(m_begin_stml);
+
+      return true;
+   }
+
+   bool commit()
+   {
+      int rc = sqlite3_step(m_commit_stml);
+      if (SQLITE_DONE != rc)
+      {
+         printf("sqlite3_step(commit) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+      }
+
+      sqlite3_clear_bindings(m_commit_stml);
+      sqlite3_reset(m_commit_stml);
+
+      return true;
+   }
+
+   bool insert(unsigned long ip, unsigned short port, int result, const char* data, size_t data_len)
+   {
+      int rc = sqlite3_bind_int64(m_insert_stml, 1, ip);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_bind_int64(ip) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+      }
+
+      rc = sqlite3_bind_int(m_insert_stml, 2, port);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_bind_int(port) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+      }
+
+      rc = sqlite3_bind_int64(m_insert_stml, 3, result);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_bind_int64(result) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+      }
+
+      rc = sqlite3_bind_text(m_insert_stml, 4, data, data_len, SQLITE_STATIC);
+      if (rc != SQLITE_OK)
+      {
+         printf("sqlite3_bind_text(response) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+      }
+
+		rc = sqlite3_step(m_insert_stml);
+		if (SQLITE_DONE != rc)
+		{
+         printf("sqlite3_step(insert) error: %s\n", sqlite3_errmsg(m_db));
+         return false;
+		}
+
+      sqlite3_clear_bindings(m_insert_stml);
+      sqlite3_reset(m_insert_stml);
+
+      return true;
+   }
+
+private:
+   sqlite3* m_db = nullptr;
+   sqlite3_stmt* m_insert_stml = nullptr;
+   sqlite3_stmt* m_begin_stml = nullptr;
+   sqlite3_stmt* m_commit_stml = nullptr;
+};
--- Scanner/main.cpp
+++ Scanner/main.cpp
@@ -9,25 +9,26 @@
 #include <openssl/err.h>
 #include "ConnSocket.hpp"
 #include "IPSpaceSweeper.hpp"
+#include "DataStore.hpp"
 
 SSL_CTX_ptr g_ssl_ctx(nullptr, SSL_CTX_free);;
 
-static void log_ssl()
-{
-   int err;
-   while (err = ERR_get_error()) {
-      char* str = ERR_error_string(err, 0);
-      if (!str)
-         return;
-      printf(str);
-      printf("\n");
-      fflush(stdout);
-   }
-}
+static bool g_keep_running = true;
 
+BOOL consoleHandler(DWORD signal) {
+
+   if (signal == CTRL_C_EVENT)
+   {
+      printf("Exiting...\n");
+      g_keep_running = false;
+   }
+   return true;
+}
 
 int main()
 {
+   SetConsoleCtrlHandler(consoleHandler, TRUE);
+
    SSL_library_init();
    SSLeay_add_ssl_algorithms();
    SSL_load_error_strings();
@@ -42,100 +43,144 @@ int main()
       return 1;
    }
 
-   const size_t concurrency = 5000;
-   std::vector<ConnSocket> socks(concurrency);
-   std::vector<WSAPOLLFD>  fdas(concurrency);
-   IPSpaceSweeper ip_range;
-
-   //ip_range.add_range("200.147.118.0", 24);
-   ip_range.add_range("200.0.0.0", 8);
-
-   const auto stat_interval = std::chrono::seconds(2);
-   const auto start = std::chrono::system_clock::now();
-   auto last_stat = start;
-
-   size_t returnedData = 0;
-   while(true)
+   try
    {
-      bool is_there_active_conn = false;
-      for (size_t i = 0; i < socks.size(); ++i)
+      const size_t concurrency = 10000;
+      std::vector<ConnSocket> socks(concurrency);
+      std::vector<WSAPOLLFD>  fdas(concurrency);
+      IPSpaceSweeper ip_range;
+
+      //ip_range.add_range("200.147.118.0", 24);
+      ip_range.add_range("200.0.0.0", 8);
+
+      DataStore datastore;
+
+      printf("Starting scan...\n");
+
+      const auto stat_interval = std::chrono::seconds(5);
+      const auto start = std::chrono::system_clock::now();
+      auto last_stat = start;
+
+      size_t returnedData = 0;
+      while (g_keep_running)
       {
-         if (socks[i].is_connected())
+         bool is_there_active_conn = false;
+         for (size_t i = 0; i < socks.size(); ++i)
          {
-            is_there_active_conn = true;
-         }
-         else if(!ip_range.has_range_finished())
-         {
-            const auto ip = ip_range.get_ip();
-            //printf("Testing %d.%d.%d.%d\n", ip & 0x000000FF, (ip & 0x0000FF00) >> 8, (ip & 0x00FF0000) >> 16, (ip & 0xFF000000) >> 24);
-            if (!socks[i].connect(ip, 443))
-            {
-               printf("Error connecting socket %zd\n", i);
-               return -1;
-            }
-            else
+            if (socks[i].is_connected())
             {
                is_there_active_conn = true;
-               fdas[i] = socks[i].get_pollfd();
+            }
+            else if (!ip_range.has_range_finished())
+            {
+               const auto ip = ip_range.get_ip();
+               //printf("Testing %d.%d.%d.%d\n", ip & 0x000000FF, (ip & 0x0000FF00) >> 8, (ip & 0x00FF0000) >> 16, (ip & 0xFF000000) >> 24);
+               if (!socks[i].connect(ip, 443))
+               {
+                  printf("Error connecting socket %zd\n", i);
+                  return -1;
+               }
+               else
+               {
+                  is_there_active_conn = true;
+                  fdas[i] = socks[i].get_pollfd();
+               }
             }
          }
-      }
 
-      if (!is_there_active_conn)
-      {
-         printf("Finished scanning\n");
-         break;
-      }
+         if (!is_there_active_conn)
+         {
+            printf("Finished scanning\n");
+            break;
+         }
 
-      socks.erase(std::remove_if(socks.begin(), socks.end(), [](const auto& it) {return !it.is_connected(); }), socks.end());
-      fdas.resize(socks.size());
-      for (int i = 0; i < socks.size(); ++i)
-      {
-         fdas[i] = socks[i].get_pollfd();
-      }
-
-      int ret = WSAPoll(fdas.data(), static_cast<ULONG>(fdas.size()), 100);
-      if (ret < 0)
-      {
-         printf("WSAPoll error - Error=%d\n", WSAGetLastError());
-         break;
-      }
-      else
-      {
+         socks.erase(std::remove_if(socks.begin(), socks.end(), [](const auto& it) {return !it.is_connected(); }), socks.end());
+         fdas.resize(socks.size());
          for (int i = 0; i < socks.size(); ++i)
          {
-            if (socks[i].process_poll(fdas[i]))
+            fdas[i] = socks[i].get_pollfd();
+         }
+
+         int ret = WSAPoll(fdas.data(), static_cast<ULONG>(fdas.size()), 100);
+         if (ret < 0)
+         {
+            printf("WSAPoll error - Error=%d\n", WSAGetLastError());
+            break;
+         }
+         else
+         {
+            if (!datastore.begin())
             {
-               auto ret = socks[i].get_result();
-               if (ret.data_len > 0)
-               {
-                  //printf("IP.%08X:%d  ->  Data(%zd)\n", ret.ip, ret.port, ret.data_len);
-                  ++returnedData;
-               }
-               socks[i].disconnect();
+               printf("Error starting data store transaction\n");
             }
+
+            for (int i = 0; i < socks.size(); ++i)
+            {
+               if (socks[i].process_poll(fdas[i]))
+               {
+                  auto ret = socks[i].get_result();
+                  if (ret.data_len > 0)
+                  {
+                     //printf("IP.%08X:%d  ->  Data(%zd)\n", ret.ip, ret.port, ret.data_len);
+                     if (!datastore.insert(ret.ip, ret.port, ret.result, ret.data, ret.data_len))
+                     {
+                        printf("Error storing raw response\n");
+                     }
+                     ++returnedData;
+                  }
+                  socks[i].disconnect();
+               }
+            }
+
+            if (!datastore.commit())
+            {
+               printf("Error commiting data store transaction\n");
+            }
+         }
+
+         if ((std::chrono::system_clock::now() - last_stat) >= stat_interval)
+         {
+            const auto [current, max_count] = ip_range.get_stats();
+            const auto percentage = (100.0 * current) / max_count;
+            const auto data_percentage = (100.0 * returnedData) / current;
+            const auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
+            const auto remaining = ((elapsed.count() * 100.0) / percentage) - elapsed.count();
+
+            printf(  "\n******************** PROGRESS ********************\n"
+                     "  Sweeped %u of %u addresses - %5.2f%%\n"
+                     "  %zd IPs returned data - %5.2f%%\n"
+                     "  Elapsed %lld seconds\n"
+                     "  Estimated remaining time: %.0f seconds\n",
+                     current, max_count, percentage,
+                     returnedData, data_percentage,
+                     elapsed.count(),
+                     remaining );
+
+            last_stat = std::chrono::system_clock::now();
          }
       }
 
-      if ((std::chrono::system_clock::now() - last_stat) >= stat_interval)
-      {
-         const auto [current, max_count] = ip_range.get_stats();
-         const float percentage = (100.0 * current) / max_count;
-         const float data_percentage = (100.0 * returnedData) / current;
-         const auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
-         const auto remaining = ((elapsed.count() * 100.0) / percentage) - elapsed.count();
+      printf("Exited main loop\n");
 
-         printf("\n******************** PROGRESS ********************\n");
-         printf("  Sweeped %u of %u addresses - %5.2f%%\n", current, max_count, percentage);
-         printf("  %zd IPs returned data - %5.2f%%\n", returnedData, data_percentage);
-         printf("  Elapsed %lld seconds\n", elapsed.count());
-         printf("  Estimated remaining time: %.0f seconds\n", remaining);
+      const auto [current, max_count] = ip_range.get_stats();
+      const auto percentage = (100.0 * current) / max_count;
+      const auto data_percentage = (100.0 * returnedData) / current;
+      const auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start);
 
-         last_stat = std::chrono::system_clock::now();
-      }
+      printf("\n******************** FINISHED ********************\n");
+      printf("  Sweeped %u of %u addresses - %5.2f%%\n", current, max_count, percentage);
+      printf("  %zd IPs returned data - %5.2f%%\n", returnedData, data_percentage);
+      printf("  Elapsed %lld seconds\n", elapsed.count());
+      printf("\n**************************************************\n");
+   }
+   catch (std::exception & e)
+   {
+      printf("EXCEPTION: %s\n", e.what());
    }
 
    WSACleanup();
+
+   system("pause");
 
    return 0;
 }
